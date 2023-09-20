dc4ab8fc654cc878561ff31262a221a95a1dff5d
Kody DeGolier <baron063@ahl04.agate.msi.umn.edu>
2023-09-20 08:01:18 -0500

    Added in the virtual environment step so CrossMap can be found

--- Crossmap_step.sh
+++ Crossmap_step.sh
@@ -1,12 +1,17 @@
-# Converting to VCF files so that CrossMap can be used
+#!/bin/bash
+# Adding in the virual environment where CrossMap is installed
+source /home/faird/shared/code/external/envs/miniconda3/load_miniconda3.sh
+conda activate GDC_pipeline
 
+module load plink
+module load python
+
+# Converting to VCF files so that CrossMap can be used
 plink --bfile DATA  --recode vcf --out DATA 
 
-# If fixed... it should be as easy as 
-# Causing errors saying that it is unable to find CrossMap.py
-# CrossMap.py vcf hg19ToHg38.over.chain.gz DATA.vcf hg38.fa out.hg38.vcf
+# Calling CrossMap
+CrossMap.py vcf hg19ToHg38.over.chain.gz DATA.vcf hg38.fa out.hg38.vcf
 
 # Converting back to bim/fam/bed format for the rest of the steps
 plink --vcf out.hg38.vcf --make-bed --out hg38_DATA
 
-# Assuming it works I'll need to convert it back into standard format...
