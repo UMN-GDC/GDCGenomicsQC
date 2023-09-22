b3b448e9ead2e00843479b223dde3e5a9a2c10c0
Kody DeGolier <baron063@ahl01.agate.msi.umn.edu>
2023-09-22 11:34:34 -0500

    fixed it so now if the reference genome sequence file is correct CrossMap will run

--- Crossmap_step.sh
+++ Crossmap_step.sh
@@ -3,7 +3,7 @@ source /home/faird/shared/code/external/envs/miniconda3/load_miniconda3.sh
 conda activate GDC_pipeline
 
 # Calling CrossMap
-CrossMap.py vcf hg19ToHg38.over.chain.gz vcf_data hg38.fa out.hg38.vcf
+CrossMap.py vcf hg19ToHg38.over.chain.gz vcf_data.vcf hg38.fa out.hg38.vcf
 
 conda deactivate # Get out of GDC_pipeline env.
 conda deactivate # Get out of DCAN_base env.
