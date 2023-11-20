96fd14c6512ed4fe073d4d0a44744d74e6af7173
Kody DeGolier <baron063@ahl03.agate.msi.umn.edu>
2023-11-20 14:26:59 -0600

    Moved other modules and adjusted the main.sh appropriately

---
+++ src/Crossmap_step.sh
@@ -0,0 +1,10 @@
+# Adding in the virual environment where CrossMap is installed
+source /home/faird/shared/code/external/envs/miniconda3/load_miniconda3.sh
+conda activate GDC_pipeline
+
+# Calling CrossMap
+CrossMap.py vcf hg19ToHg38.over.chain.gz vcf_data.vcf hg38.fa out.hg38.vcf
+
+conda deactivate # Get out of GDC_pipeline env.
+conda deactivate # Get out of DCAN_base env.
+
