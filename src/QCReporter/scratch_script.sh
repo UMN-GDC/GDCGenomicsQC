dedec556aa45ee8d3aaf850948c7ad269cb06415
Kody DeGolier <baron063@ahl04.agate.msi.umn.edu>
2023-12-15 11:23:11 -0600

    Updated error tracking and stringency for using this tool

--- src/QCReporter/scratch_script.sh
+++ src/QCReporter/scratch_script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#Rscript logReader.R /sampleLogs/first_pass.log #Updated error detecting works
+Rscript logReader.R /sampleLogs/first_pass.log mind #Default output file name works
 Rscript logReader.R /sampleLogs/first_pass.log mind test1.txt #Works
 Rscript logReader.R /sampleLogs/step2_temp.log geno test2.txt #Works
 Rscript logReader.R /sampleLogs/gender_check.log check-sex test3.txt #Works
 Rscript logReader.R /sampleLogs/SMILES_done_MAF.log maf test4.txt #Works
 Rscript logReader.R /sampleLogs/Step7_temp1.log filter-founders test5.txt #Works 
 Rscript logReader.R /sampleLogs/Step5_temp.log hwe test6.txt #Works
-#Rscript logReader.R /sampleLogs/gender_check.log mind test5.txt #For testing error messages.. This error message makes sense 
-#Rscript logReader.R /sampleLogs/bad_log.log mind test6.txt #For testing error messages.. This error message makes sense
+Rscript logReader.R /sampleLogs/gender_check.log mind test5.txt #For testing error messages.. This error message makes sense 
+Rscript logReader.R /sampleLogs/bad_log.log mind test6.txt #For testing error messages.. This error message makes sense
 
 #Plink options I am unsure of... Probably not used in the pipeline...
 # --mpheno 4 [SMILES_pheno_updated.log
