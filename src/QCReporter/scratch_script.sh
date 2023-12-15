19ea1351d91f4101e7ba0a57bae81ccbf41db024
Kody DeGolier <baron063@ahl04.agate.msi.umn.edu>
2023-12-15 11:05:02 -0600

    Added in ability to read from filter founder and hwe logs

--- src/QCReporter/scratch_script.sh
+++ src/QCReporter/scratch_script.sh
@@ -2,7 +2,19 @@
 
 Rscript logReader.R /sampleLogs/first_pass.log mind test1.txt #Works
 Rscript logReader.R /sampleLogs/step2_temp.log geno test2.txt #Works
-#Rscript logReader.R /sampleLogs/first_pass.log check-sex test3.txt
-Rscript logReader.R /sampleLogs/SMILES_done_MAF.log maf test4.txt #Doesn't like the second part of it... 
-#Rscript logReader.R /sampleLogs/gender_check.log mind test5.txt #This error message makes sense
-#Rscript logReader.R /sampleLogs/bad_log.log mind test6.txt #This error message makes sense
+Rscript logReader.R /sampleLogs/gender_check.log check-sex test3.txt #Works
+Rscript logReader.R /sampleLogs/SMILES_done_MAF.log maf test4.txt #Works
+Rscript logReader.R /sampleLogs/Step7_temp1.log filter-founders test5.txt #Works 
+Rscript logReader.R /sampleLogs/Step5_temp.log hwe test6.txt #Works
+#Rscript logReader.R /sampleLogs/gender_check.log mind test5.txt #For testing error messages.. This error message makes sense 
+#Rscript logReader.R /sampleLogs/bad_log.log mind test6.txt #For testing error messages.. This error message makes sense
+
+#Plink options I am unsure of... Probably not used in the pipeline...
+# --mpheno 4 [SMILES_pheno_updated.log
+# same with [SMILES_matched_to_b38_forward.log]
+# [SMILES_comm.log] <-- FRAPOSA
+# --het [pruned_data.log] <-- doesn't really give new information
+# --extract [pihat_min0.2_in_founders.log] <-- unsure what it adds
+# indepSNP.log <-- This one looks useful just difficult to incorporate. Do we want each chromosome information?
+# Unsure about [hgdp_v1.log]
+# 
