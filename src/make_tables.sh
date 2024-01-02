daa1bddeefd2adf06188860abdaaa4f0920ceb40
Kody DeGolier <baron063@ahl03.agate.msi.umn.edu>
2024-01-02 10:42:51 -0600

    fixed extended version of logReader and updated make_tables.sh

--- src/make_tables.sh
+++ src/make_tables.sh
@@ -3,7 +3,7 @@
 show_help() {
   echo "Usage: $0 [options]"
   echo "Options:"
-  echo "  --FILE <file_name>  Specify the file to process. Must be a .bed file"
+  echo "  --FILE <file_name>  Specify the file to gather QC information after running main.sh. Must be a .bed file"
   echo "  --help              Display this help message."
 }
 
@@ -27,13 +27,6 @@ while [[ $# -gt 0 ]]; do
         show_help
         exit 0
       ;;
-          *) 
-            echo "Please enter a valid option (Yes/No) when choosing to include the UPDTSO flag."
-            exit 0
-          ;;
-        esac
-        shift 2
-      ;;
     *)
       echo "Unrecognized option: $key"
       show_help
@@ -43,7 +36,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 
-# Just in case they included the file extension
+# Just in case the file extension is included
 FILE=${FILE%*}
 
 
@@ -54,14 +47,17 @@ Rscript logReader.R ./${FILE}_3.log geno QC4_geno.txt
 Rscript logReader.R ./${FILE}_4.log mind QC5_mind.txt
 Rscript logReader.R ./gender_check.log check-sex QC6_sex_check.txt
 Rscript logReader.R ./${FILE}_6_MAF.log maf QC_7_maf.txt
-## Made it to QC step 8 ##
+Rscript logReader.R ./${FILE}_7a.log hwe QC_8_hwe.txt
+Rscript logReader.R ./${FILE}_7.log hwe QC_8b_hwe.txt
+Rscript logReader.R ./${FILE}_9a.log filter-founders QC_9_filter-founders.txt
 
+Rscript logReader_extended.R ./indepSNP.log indep-pairwise QC_indep_pairwise.txt
 
 #for troubleshooting and reference
-Rscript logReader.R /sampleLogs/first_pass.log mind #Default output file name works
-Rscript logReader.R /sampleLogs/first_pass.log mind test1.txt #Works
-Rscript logReader.R /sampleLogs/step2_temp.log geno test2.txt #Works
-Rscript logReader.R /sampleLogs/gender_check.log check-sex test3.txt #Works
-Rscript logReader.R /sampleLogs/SMILES_done_MAF.log maf test4.txt #Works
-Rscript logReader.R /sampleLogs/Step7_temp1.log filter-founders test5.txt #Works 
-Rscript logReader.R /sampleLogs/Step5_temp.log hwe test6.txt #Works
+#Rscript logReader.R /sampleLogs/first_pass.log mind #Default output file name works
+#Rscript logReader.R /sampleLogs/first_pass.log mind test1.txt #Works
+#Rscript logReader.R /sampleLogs/step2_temp.log geno test2.txt #Works
+#Rscript logReader.R /sampleLogs/gender_check.log check-sex test3.txt #Works
+#Rscript logReader.R /sampleLogs/SMILES_done_MAF.log maf test4.txt #Works
+#Rscript logReader.R /sampleLogs/Step7_temp1.log filter-founders test5.txt #Works 
+#Rscript logReader.R /sampleLogs/Step5_temp.log hwe test6.txt #Works
