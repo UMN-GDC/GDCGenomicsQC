6f14b67fc39b5553c2b4787145edd074f6fc654f
Kody DeGolier <baron063@ahl02.agate.msi.umn.edu>
2024-01-25 10:39:53 -0600

    Added in ability to generate a text file from genomeharmonizer .log files

--- src/QC_report.R
+++ src/QC_report.R
@@ -2,14 +2,109 @@
 
 #These two files are created and overwrote whenever there is a --missing call
 # For Longs QC this is at the end of QC steps
-wd=getwd()
+setwd("/panfs/jay/groups/16/saonli/baron063/R")
+
+suppressMessages(library(tidyverse))
+
+#Adding in ability for script to accept command line arguments
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least two arguments: if not, return an error
+if (length(args)<=1) {
+  stop("A path to the data and the file name for your target population need to be provided.", call.=FALSE)
+} else if (length(args)>=2) {
+  args[1] -> base_location #Place where the logs are located
+  # default output file
+  args[2] -> filename #What Output_TLPRS_best.PRS is called
+  if(length(args)==2){
+    output_name <- "default_name"
+  }
+  args[3] -> output_name
+}
+
+wd=base_location
+print(wd)
+print(filename)
+print(output_name)
+
+#Back to where the data is
+setwd(wd)
+
 indmiss<-read.table(file="plink.imiss", header=TRUE)
 snpmiss<-read.table(file="plink.lmiss", header=TRUE)
 # read data into R 
 
+#Reading in the tables for later use
+QC2_geno_table <- read.table(QC2_geno.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC3_mind_table <- read.table(QC3_mind.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC4_geno_table <- read.table(QC4_geno.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC5_mind_table <- read.table(QC5_mind.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC6_sex_check_table <- read.table(QC6_sex_check.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC7_maf_table <- read.table(QC7_maf.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC8_hwe_table <- read.table(QC8_hwe.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC8b_hwe_table <- read.table(QC8b_hwe.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC9_filter_founders_table <- read.table(QC9_filter-founders.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+
+QC_indep_pairwise_table <- read.table(QC_indep_pairwise.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+QC_indep_pairwise_bychr <- read.table(each_SNP_QC_indep_pairwise.txt, header = T, sep = " ", col.names = T, row.names = F, quote = F)
+
+#Key 
+# common            InSubjects InMale InFemale InSNPs 
+# common            OutSubjects *unique* OutSNPs
+#
+# Start of unique element based on plink option chosen
+# --mind            NumPeopleRemoved
+# --geno            NumVariantsRemoved
+# --check-sex       Numx NumY NumProblems
+# --maf             NumVariantsRemoved
+# --filter-founders NumRemoved NumFounder NumNonFounder
+# --hwe             NumVariantsRemoved
+#
+# Below is from the logReader_extended function
+# --indep-pairwise  NumSNPStoPrune <-- table1
+#                   PrunedSNPS Chr RemainingSNPS <-- table2
+
+pdf("QCreport_2.pdf")
+geno_table_summary = rbind(QC2_geno_table, QC4_geno_table)
+# Probably will turn it into a gt?
+# Would be good to rearrange the columns? 
+# These all have 7 columns
+mind_table_summary = rbind(QC3_mind_table, QC5_mind_table)
+
+QC6_sex_check_table
+# Turn it into a gt?
+
+QC7_maf_table
+# gt?
+
+hwe_check_table = rbind(QC8_hwe_table, QC8b_hwe_table)
+# gt?
+
+QC9_filter_founders_table
+# gt?
+
+dev.off() # Ends pdf creation. 
+
 pdf("QCreport.pdf") #indicates pdf format and gives title to file
+
+data.frame("Subject" = 1:length(indmiss),
+          "Missingness" = indmiss[,6]) %>%
+  ggplot(aes(x = Missingness)) +
+  geom_histogram() + 
+  geom_vline(xintercept = 0.15, color = "red") + 
+  xlab("Missingness per subject") +
+  ggtitle("% SNPS missing per subject")
 hist(indmiss[,6],main="Histogram individual missingness", xlab = "Proportion of missing SNPs") #selects column 6, names header of file
 
+
+data.frame("SNP" = 1:length(snpmiss),
+          "Missingness" = snpmiss[,5]) %>%
+  ggplot(aes(x = Missingness)) +
+  geom_histogram() +
+  geom_vline(xintercept = 0.15, color = "red") +
+  xlab("Missingness per SNP") +
+  ggtitle("% calls missing per SNP")
 hist(snpmiss[,5],main="Histogram SNP missingness", xlab = "Proportion of sample missing for each SNP")  
 
 # print("hist_miss.R Script Success!")
@@ -33,12 +128,26 @@ barplot(temptab, main = "Homozygosity Analysis", xlab = "Status")
 
 maf_freq <- read.table("MAF_check.frq", header =TRUE, as.is=T)
 hist(maf_freq[,5],main = "MAF distribution", xlab = "MAF")
+data.frame("SNP" = 1:length(maf_freq),
+           "MAF" = maf_freq[,5]) %>%
+  ggplot(aes(x = MAF)) +
+  geom_histogram() +
+  geom_vline(xintercept = 0.05, color = "red") +
+  xlab("Minor allele frequency") +
+  ggtitle("MAF distribution")
 
 # print("MAF_check.R Script Success!")
 
 
 hwe<-read.table (file="plink.hwe", header=TRUE)
 hist(hwe[,9],main="Histogram HWE", xlab = "P-value")
+data.frame("SNP" = 1:length(hwe),
+          "HWE" = hwe[,9]) %>%
+          ggplot(aes(x = HWE)) +
+          geom_histogram() +
+          geom_vline(xintercept = -0.05, color = "red") +
+          xlab("log(HWE p-value)") +
+          ggtitle("Hardy-Weinberg Equilibrium p-value distribution")
 
 hwe_zoom<-read.table (file="plinkzoomhwe.hwe", header=TRUE)
 hist(hwe_zoom[,9],main="Histogram HWE: strongly deviating SNPs only", xlab = "P-value")
@@ -47,6 +156,13 @@ hist(hwe_zoom[,9],main="Histogram HWE: strongly deviating SNPs only", xlab = "P-
 
 het <- read.table("R_check.het", head=TRUE)
 het$HET_RATE = (het$"N.NM." - het$"O.HOM.")/het$"N.NM."
+data.frame("Subject" = 1:length(het),
+          "Heterozygosity" = het$HET_RATE) %>%
+  ggplot(aes(x = Heterozygosity)) +
+  geom_histogram() + 
+  geom_vline(xintercept = c(-2, 2), color = "red") + 
+  xlab("Heterozygosity F statistic") +
+  ggtitle("Heterozygous F statistic distribution")
 hist(het$HET_RATE, xlab="Heterozygosity Rate", ylab="Frequency", main= "Heterozygosity Rate")
 
 
--- src/generate_report.sh
+++ src/generate_report.sh
@@ -2,6 +2,7 @@
 
 cd ./src/QCReporter/
 
-quarto render QCReport.qmd
+quarto render simple.qmd
+# quarto render QCReport.qmd #Currently not working
 
 echo "Report has been successfully generated"
