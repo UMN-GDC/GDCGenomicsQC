fc16f2ce9f7552f018fe281505c053a694561743
Kody DeGolier <baron063@ahl01.agate.msi.umn.edu>
2024-02-20 11:19:08 -0600

    Fixes to allow usage of quarto for the automated report

--- src/generate_report.sh
+++ src/generate_report.sh
@@ -3,9 +3,10 @@
 work_dir=$(pwd)
 echo ${work_dir}
 
-cd ./QCReporter/
+cd ./src/QCReporter/
 
-quarto render simple.qmd
+quarto render test_simpler.qmd
+# quarto render simple.qmd #Currently not working
 # quarto render QCReport.qmd #Currently not working
 
 echo "Report has been successfully generated"
