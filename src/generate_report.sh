be14ab13ada1a5169fa0161e48d39177ab542ec6
Kody DeGolier <baron063@ahl04.agate.msi.umn.edu>
2024-03-01 10:46:15 -0600

    Updates include bug fixes that allow the qmd script to run without errors from command line

--- src/generate_report.sh
+++ src/generate_report.sh
@@ -5,8 +5,8 @@ echo ${work_dir}
 
 cd ./src/QCReporter/
 
-quarto render test_simpler.qmd
+quarto render test_simpler.qmd -P path_to_data: '/home/gdc/shared/GDC_pipeline/data/'
 # quarto render simple.qmd #Currently not working
-quarto render QCReport.qmd -P path_to_data: '/home/gdc/shared/GDC_pipeline/logs/'
+# quarto render QCReport.qmd -P path_to_data: '/home/gdc/shared/GDC_pipeline/logs/'
 
 echo "Report has been successfully generated"
