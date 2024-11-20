20da9ba8c99aee229ad705f3e4170755169919dd
Kody DeGolier <baron063@agd03.agate.msi.umn.edu>
2024-11-20 10:45:42 -0600

    Updates to fix creation of sbatch wrapper and incorporate rfmix options

--- src/create_main_wrapper.sh
+++ src/create_main_wrapper.sh
@@ -8,9 +8,9 @@ user_x500=$4
 desired_working_directory=$5
 using_crossmap=$6
 using_genome_harmonizer=$7
-using_rfmix=$8
-making_report=$9
-custom_qc=${10}
+making_report=$8
+custom_qc=$9
+using_rfmix=${10}
 
 output=${desired_working_directory}/${input_file_name}_wrapper.sh
 mkdir -p ${desired_working_directory}
@@ -22,6 +22,6 @@ sed -i 's@x500@'${user_x500}'@' ${output}
 sed -i 's@WK@'${desired_working_directory}'@' ${output} 
 sed -i 's@CRSMP@'${using_crossmap}'@' ${output} 
 sed -i 's@GNHRM@'${using_genome_harmonizer}'@' ${output}
-sed -i 's@RFMX@'${using_rfmix}'@' ${output} 
 sed -i 's@RPT@'${making_report}'@' ${output}
 sed -i 's@CSTQC@'${custom_qc}'@' ${output}
+sed -i 's@RFMX@'${using_rfmix}'@' ${output} 
