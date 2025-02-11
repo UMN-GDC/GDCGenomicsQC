f4f99012c0ac5c2954767794af0d760ad1487a61
Michael Anderson <52722296+and02709@users.noreply.github.com>
2025-02-11 14:23:40 -0600

    Update main_template.sh
    Reversed truncated instructions

--- src/main_template.sh
+++ src/main_template.sh
@@ -77,3 +77,82 @@ fi
 ########################################################################################################
 
 
+######################################## Pedigree ######################################################
+echo "(Step 3) Relatedness check"
+${path_to_repo}/src/run_primus.sh ${WORK} ${REF} ${NAME} ${path_to_repo} ${DATATYPE}
+#########################################################################################################
+
+
+######################################## Ethnicity ######################################################
+echo "(Step 4) PCA"
+if [ ${rfmix_option} -eq 1 ]; then
+  ## requires a text file that has all of the flags and specifications
+  sbatch --wait ${path_to_repo}/src/run_rfmix.sh ${WORK} ${REF} ${NAME} ${path_to_repo}
+else # Alternative behavior
+  ${path_to_repo}/src/run_fraposa.sh ${WORK} ${REF} ${NAME} ${path_to_repo}
+fi
+#########################################################################################################
+
+
+################### Subset data based on Ethnicity and Rerun QC (Step 2) on the subsets #################
+cd ${WORK}
+if [ ${rfmix_option} -eq 1 ]; then
+  ETHNICS=$(awk '{print $3}' ${WORK}/PCA/study.${NAME}.unrelated.comm.popu | sort | uniq)
+else # Alternative behavior
+  ETHNICS=$(awk -F'\t' '{print $3}' ${WORK}/PCA/study.${NAME}.unrelated.comm.popu | sort | uniq)
+fi
+
+for DATATYPE in ${ETHNICS}; do
+    plink --bfile ${WORK}/aligned/study.${NAME}.lifted.aligned --keep ${WORK}/PCA/${DATATYPE} --make-bed --out ${WORK}/aligned/study.${NAME}.${DATATYPE}.lifted.aligned
+    if [ ${custom_qc} -eq 1 ]; then
+    ## Will follow a pre-determined naming such as ${WORK}/custom_qc.SLURM
+      sbatch ${WORK}/custom_qc.SLURM ${WORK}/aligned/study.${NAME}.${DATATYPE}.lifted.aligned ${DATATYPE} ${path_to_repo}
+    else # Default behavior
+      sbatch ${path_to_repo}/src/standard_QC.job ${WORK}/aligned/study.${NAME}.${DATATYPE}.lifted.aligned ${DATATYPE} ${path_to_repo}
+    fi
+done
+###########################################################################################################
+
+##Putting in to wait until the jobs are done
+jobs_remaining=$(squeue --me | grep QC | wc -l)
+echo "${jobs_remaining} jobs remaining at the start of this waiting loop"
+x=1
+while [ ${jobs_remaining} -gt 0 ]
+do
+  sleep 1m
+  jobs_remaining=$(squeue --me | grep QC | wc -l)
+  echo "${jobs_remaining} after waiting for ${x} minutes"
+  ((x++))
+done
+  
+########################## Restructuring and cleaning up for the report writer ############################
+#1. move over png and .popu file from PCA directory into the 'full' directory
+cp ${WORK}/PCA/study.${NAME}*popu ${WORK}/full/
+cp ${WORK}/PCA/study.${NAME}*png ${WORK}/full/
+
+#2. move the genome_harmonizer_full_log.txt into the 'full' directory
+cp ${WORK}/aligned/*harmonizer*.txt ${WORK}/full/
+primus_file=$(find ${WORK} -type f -name "full.QC8_cleaned.genome")
+cp -v ${primus_file} ${WORK}/full/primus_file.genome
+
+#3. move other directories into a temporary location called 'temp'
+# aligned, lifted, logs, PCA, relatedness, relatedness_OLD
+mkdir ${WORK}/temp
+mv -f ${WORK}/aligned ${WORK}/temp/
+mv -f ${WORK}/lifted ${WORK}/temp/
+mv -f ${WORK}/logs ${WORK}/temp/
+mv -f ${WORK}/phased ${WORK}/temp/
+mv -f ${WORK}/rfmix ${WORK}/temp/
+mv -f ${WORK}/PCA ${WORK}/temp/
+mv -f ${WORK}/relatedness ${WORK}/temp/
+mv -f ${WORK}/relatedness_OLD ${WORK}/temp/
+mv -f ${WORK}/*.out ${WORK}/logs/out/
+mv -f ${WORK}/*.err ${WORK}/logs/errors/
+
+rm ${WORK}/*.lifted* #To clean up the working directory of unnecessary files 
+
+#4. execute run_generate_reports.sh ## Need to make this optional ##
+module load R/4.4.0-openblas-rocky8
+if [ ${report_writer} -eq 1 ]; then
+  ${path_to_repo}/src/run_generate_reports.sh ${WORK} ${path_to_repo}
+fi
