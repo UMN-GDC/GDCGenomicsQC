328f765fdcd33e3d5b157949f562859dfb10ac04
Kody DeGolier <baron063@cn1164.agate.msi.umn.edu>
2025-05-28 11:45:35 -0500

    Updates to swap out the reference locations for enhanced accessibility

--- src/run_phase.sh
+++ src/run_phase.sh
@@ -15,6 +15,6 @@ NAME=$3
 path_to_repo=$4
 
 mkdir $WORK/phased
-sbatch --time 4:00:00 --mem 16GB --array 1-22 --wait -N1 ${path_to_repo}/src/phase_individual.sh ${WORK} ${NAME}
+sbatch --time 4:00:00 --mem 16GB --array 1-22 --wait -N1 ${path_to_repo}/src/phase_individual.sh ${WORK} ${NAME} ${REF}
 
 
