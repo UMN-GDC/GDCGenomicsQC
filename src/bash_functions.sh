d45f07a55fd5b0199b8c1352e31f5ae5339e7f41
Kody DeGolier <baron063@acn31.agate.msi.umn.edu>
2025-07-16 14:14:38 -0500

    Additions to help maintain functionality

--- src/bash_functions.sh
+++ src/bash_functions.sh
@@ -332,8 +332,8 @@ subset_ancestries_run_standard_qc() {
     if [ ${custom_qc} -eq 1 ]; then
     ## Will follow a pre-determined naming such as ${WORK}/custom_qc.SLURM
       sbatch ${WORK}/custom_qc.SLURM ${WORK}/aligned/study.${NAME}.${DATATYPE}.lifted.aligned ${DATATYPE} ${path_to_repo}
-    else # Default behavior
-      sbatch ${path_to_repo}/src/standard_QC.job ${WORK}/aligned/study.${NAME}.${DATATYPE}.lifted.aligned ${DATATYPE} ${path_to_repo}
+    else # Default behavior      
+      sbatch ${path_to_repo}/src/per_ancestry_QC.job ${WORK} ${DATATYPE} ${path_to_repo}
     fi
   done
 }
