c181507aec4ada0613d5f058cdd60f9d10945f00
Michael Anderson <52722296+and02709@users.noreply.github.com>
2025-07-17 16:25:54 -0500

    Update run_crossmap.sh

--- src/run_crossmap.sh
+++ src/run_crossmap.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16GB
+#SBATCH --mem=32GB
 #SBATCH --time=2:00:00
 #SBATCH -p agsmall
 #SBATCH -o crossmap.out
