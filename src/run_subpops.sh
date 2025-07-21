36ed92f9f247f5fdd614bbedb6fad0e3f9891b1b
Michael Anderson <52722296+and02709@users.noreply.github.com>
2025-07-21 15:30:36 -0500

    Update run_subpops.sh

--- src/run_subpops.sh
+++ src/run_subpops.sh
@@ -14,6 +14,20 @@ REF=$2
 NAME=$3
 path_to_repo=$4
 
+module load R/4.4.0-openblas-rocky8
+
+Rscript ${path_to_repo}/src/gai2.R ${WORK} ${NAME}
+
+echo "Potential duplication below"
+mkdir $WORK/PCA
+cp $WORK/study.$NAME.unrelated.comm.popu $WORK/PCA/study.$NAME.unrelated.comm.popu
+cd $WORK/PCA
+
+# awk -F '\t' '{print $3}' $WORK/study.$NAME.unrelated.comm.popu | sort | uniq -c > subpop.txt
+awk '{print $3}' study.$NAME.unrelated.comm.popu | sort | uniq -c > subpop.txt
+awk '{print $1 "\t" $2 "\t" $3}' study.$NAME.unrelated.comm.popu > data.txt
+Rscript ${path_to_repo}/src/subpop.R ${WORK} ${NAME}
+
 mkdir $WORK/PCA
 cp $WORK/study.$NAME.unrelated.comm.popu $WORK/PCA/study.$NAME.unrelated.comm.popu
 cd $WORK/PCA
