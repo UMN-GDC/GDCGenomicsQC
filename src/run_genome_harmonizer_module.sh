98dfc58370072d49aebbb0670f9aac1db21dca79
Kody DeGolier <baron063@acn87.agate.msi.umn.edu>
2025-08-19 13:23:17 -0500

    Adjusting time limit to align with upper bound of sbatch inside of the script

--- src/run_genome_harmonizer_module.sh
+++ src/run_genome_harmonizer_module.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16GB
-#SBATCH --time=2:00:00
+#SBATCH --time=25:00:00
 #SBATCH -p agsmall
 #SBATCH -o GenotypeHarmonizer.out
 #SBATCH -e GenotypeHarmonizer.err
