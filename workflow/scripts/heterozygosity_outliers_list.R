bd921117863599e7ffcedff481131f26f6f8ec7b
coffm049 <coffm049@umn.edu>
2026-05-05 20:36:04 -0500

    lkjads

--- workflow/scripts/heterozygosity_outliers_list.R
+++ workflow/scripts/heterozygosity_outliers_list.R
@@ -2,9 +2,8 @@ args <- commandArgs(trailingOnly = TRUE)
 het_file <- args[1]
 output_dir <- args[2]
 
-het <- read.table(het_file, head=TRUE)
-print(colnames(het))
-het$HET_RATE = (het$"N(NM)" - het$"O(HOM)")/het$"N(NM)"
+het <- read.table(het_file, head=FALSE, col.names=c("FID", "IID", "O_HOM", "E_HOM", "N_NM"))
+het$HET_RATE = (het$N_NM - het$O_HOM) / het$N_NM
 het_fail = subset(het, (het$HET_RATE < mean(het$HET_RATE)-3*sd(het$HET_RATE)) | (het$HET_RATE > mean(het$HET_RATE)+3*sd(het$HET_RATE)));
 if (nrow(het_fail) > 0) {
     het_fail$HET_DST = (het_fail$HET_RATE-mean(het$HET_RATE))/sd(het$HET_RATE);
