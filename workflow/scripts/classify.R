fe70ce428c2b92344d7891405c9a70ed933ffd3e
Christian Coffmann <coffm049@ad.ahc.umn.edu>
2026-06-23 21:46:07 -0500

    add FID column to keep_*.txt files for plink2 --keep compatibility

--- workflow/scripts/classify.R
+++ workflow/scripts/classify.R
@@ -48,12 +48,16 @@ for (anc in all_ancestries) {
     result |>
         filter(.data[[predicted_col]] == anc, .data[[confidence_col]] >= args$threshold) |>
         select(IID) |>
+        mutate(FID = IID) |>
+        select(FID, IID) |>
         write_delim(file.path(args$out, paste0("keep_", anc, ".txt")), delim = "\t", col_names = FALSE)
 }
 
 result |>
     filter(.data[[confidence_col]] < args$threshold | .data[[predicted_col]] == "uncertain" | is.na(.data[[confidence_col]])) |>
     select(IID) |>
+    mutate(FID = IID) |>
+    select(FID, IID) |>
     write_delim(file.path(args$out, "keep_Other.txt"), delim = "\t", col_names = FALSE)
 
 
