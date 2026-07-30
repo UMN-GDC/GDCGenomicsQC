b8b1f7efc6f8221f311303c1beaa6c8949a33c5f
Christian <coffm049@umn.edu>
2026-07-30 14:38:36 -0500

    Fix .king.id mismatch and add expected kinship lines to relatedness plot

--- workflow/scripts/plotRelatedness.R
+++ workflow/scripts/plotRelatedness.R
@@ -5,13 +5,14 @@ kin_file <- args[1]
 kinid_file <- args[2]
 out <- args[3]
 
-ids <- read_table(kinid_file, col_names = c("FID", "IID")) |>
-  mutate(idx = row_number()) |>
-  unite(ID, FID, IID, sep = "_")
-
 lines <- readLines(kin_file)
 lower <- lapply(strsplit(lines, "\\s+"), as.numeric)
 n <- length(lower)
+
+id_lines <- readLines(kinid_file)
+ids <- read.table(text = id_lines[1:n], col.names = c("FID", "IID")) |>
+  unite(ID, FID, IID, sep = "_")
+
 K <- matrix(NA, n, n)
 for (i in seq_len(n)) {
   K[i, 1:i] <- lower[[i]]
@@ -26,14 +27,23 @@ kin <- as.data.frame(K) |>
   mutate(pair = paste(pmin(ID1, ID2), pmax(ID1, ID2), sep = "_")) |>
   distinct(pair, .keep_all = TRUE)
 
+expected_lines <- data.frame(
+  kinship = c(0.25, 0.125, 0.0625, 0.03125),
+  label = c("1st-degree", "2nd-degree", "3rd-degree", "4th-degree")
+)
+
 kin |>
   ggplot(aes(x = KINSHIP)) +
   geom_histogram(bins = 50, fill = "steelblue", color = "white") +
+  geom_vline(data = expected_lines, aes(xintercept = kinship),
+             color = "gray50", linetype = "dotted") +
+  geom_text(data = expected_lines, aes(x = kinship, label = label),
+            y = 0, hjust = -0.1, vjust = 1.5, size = 3, color = "gray50") +
   geom_vline(xintercept = 0.0442, color = "red", linetype = "dashed") +
   geom_vline(xintercept = 0.0884, color = "darkred", linetype = "dashed") +
   geom_vline(xintercept = 0.354, color = "orange", linetype = "dashed") +
   annotate("text", x = 0.05, y = 0, label = "2nd-degree", hjust = 0, vjust = 0, size = 3, color = "red") +
   annotate("text", x = 0.09, y = 0, label = "3rd-degree", hjust = 0, vjust = 0, size = 3, color = "darkred") +
   labs(x = "KING kinship coefficient", y = "Count",
-       caption = "Dashed lines at relatedness thresholds: 2nd-degree, 3rd-degree")
+       caption = "Dotted: expected kinship | Dashed: filtering thresholds")
 ggsave(out, width = 9, height = 5)
