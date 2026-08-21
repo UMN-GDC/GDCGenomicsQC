908b248b60d8536e8a9a9639c2549fae6adde683
Christian <coffm049@umn.edu>
2026-08-21 08:37:35 -0500

    Fix heterozygosity_outliers_list.R: use header=TRUE to correctly parse PLINK2 .het header

--- workflow/scripts/heterozygosity_outliers_list.R
+++ workflow/scripts/heterozygosity_outliers_list.R
@@ -2,7 +2,7 @@ args <- commandArgs(trailingOnly = TRUE)
 het_file <- args[1]
 output_dir <- args[2]
 
-het <- read.table(het_file, head=FALSE, col.names=c("FID", "IID", "O_HOM", "E_HOM", "OBS_CT", "F"))
+het <- read.table(het_file, header=TRUE, comment.char="#")
 het_fail = subset(het, (het$F < mean(het$F)-3*sd(het$F)) | (het$F > mean(het$F)+3*sd(het$F)));
 if (nrow(het_fail) > 0) {
     het_fail$F_DST = (het_fail$F - mean(het$F)) / sd(het$F);
